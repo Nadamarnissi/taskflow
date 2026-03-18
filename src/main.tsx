7ef5c85156a9364e4d6d83b8ca725ff0fdc79051
marnissi <n.marnissi@itroadgroup.com>
2026-03-18 11:24:44 +0000

    third commit

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { AuthProvider } from './features/auth/AuthContext';
 import { createRoot } from 'react-dom/client';
 import { StrictMode } from 'react';
